fix mean_step calculation in _d and _sim versions

baselines/MCTS/save_1/main1.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 from utils import discount_with_dones, Scheduler, make_path, find_trainable_variables
 from models import model, model2, model3, load, save
 import scipy.sparse as sp
-from sl_buffer_d import slBuffer_allFile
-from mct_d import MCT
+from sl_buffer1 import slBuffer_allFile
+from mct1 import MCT
 from status import Status
 
 """
@@ -221,7 +221,7 @@ def main():
     import argparse
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--save_dir', type = str, help='where is the model saved', default="GPO2/Small5")
+    parser.add_argument('--save_dir', type = str, help='where is the model saved', default="GPO2/Small6_step1")
     parser.add_argument('--best_model', type = int, help="the index of the best model (-1 for unknown)", default = -1)
     parser.add_argument('--status_file', type = str, help="which file keeps a record of the status", default = "status.pkl")
     parser.add_argument('--result_file', type = str, help="this file keeps the performance of models on testing files", default = "result.pkl")

baselines/MCTS/save_1/mct1.py
@@ -2,7 +2,6 @@ import os, time, pickle
 import numpy as np
 import scipy.sparse as sp
 from minisat.minisat.gym.GymSolver import sat
-from sl_buffer import slBuffer
 
 def softmax(x):
     e_x = np.exp(x - np.max(x))
@@ -32,19 +31,20 @@ def get_nrepeat_count(action, nact):
             start = i
             base = action[i]
     counts[base] = action.shape[0] - start
-#    print(counts)
     return counts
 
-def analyze_Pi_graph_dump(Pi_node, sl_Buffer, standard):
+def analyze_Pi_graph_dump(file_no, Pi_node, sl_Buffer, standard):
     if not Pi_node.evaluated: return # this is the finished node (no state, not evaluated)
     for act in Pi_node.children:
-        analyze_Pi_graph_dump(Pi_node.children[act], sl_Buffer, standard)
+        analyze_Pi_graph_dump(file_no, Pi_node.children[act], sl_Buffer, standard)
     # save this node's infor TODO: should the score be more quatified?
     av = Pi_node.score / Pi_node.repeat 
-    if av > standard[1]: score = - (av-standard[1]) / (standard[2]-standard[1])
-    elif av < standard[1]: score = (standard[1]-av) / (standard[1]-standard[0])
-    else: score = 0
-    sl_Buffer.add(Pi_node.state, Pi_node.Pi, score, Pi_node.repeat)
+    # for mct2.py, change the score calculation to tanh
+    score = np.tanh((standard[1] - av) * 3.0 / standard[1])
+    # if av > standard[1]: score = - (av-standard[1]) / (standard[2]-standard[1])
+    # elif av < standard[1]: score = (standard[1]-av) / (standard[1]-standard[0])
+    # else: score = 0
+    sl_Buffer.add(file_no, Pi_node.state, Pi_node.Pi, score, Pi_node.repeat)
 
 class Pi_struct(object):
     """
@@ -164,8 +164,6 @@ class MCT(object):
             return None
         while True:
             if not self.Pi_current.evaluated:
-#                if not self.phase:
-#                    print("simulation -----------------------------------------------{} at level {}".format(self.Pi_current.repeat, self.Pi_current.level))
                 needEnv = True
                 while needEnv or needSim:
                     if needEnv:
@@ -181,7 +179,6 @@ class MCT(object):
             assert next_act >= 0, "next_act is neg in file " + str(self.file_no)
             isDone, self.state = self.env.step(next_act) # there is a guarantee that this next_act is valid
             if isDone or self.Pi_current.level >= self.resign:
-#                print("step {} to DONE or resign +++++++++++++++++++++++++++++++++++++++++++++++++++++".format(next_act))
                 if self.Pi_current.level < self.min_step: self.min_step = self.Pi_current.level # update score range for this sat prob
                 if self.Pi_current.level > self.max_step: self.max_step = self.Pi_current.level
                 if (self.Pi_current.set_next(self.Pi_current.level * self.Pi_current.nrepeats[next_act]) < 0): # write back the total score from this leaf node
@@ -190,14 +187,13 @@ class MCT(object):
                 self.Pi_current = self.Pi_root
                 self.state = self.env.resetAt(self.file_no)
             else:
-#                print("step {} is continuing***********************************************".format(next_act))
                 self.Pi_current = self.Pi_current.children[next_act]
                 self.Pi_current.add_state(self.state)
 
     def write_data_to_buffer(self, sl_Buffer):
         if self.Pi_root is None: return # there is nothing to write
         standard = (self.min_step, self.Pi_root.score / self.Pi_root.repeat, self.max_step)
-        analyze_Pi_graph_dump(self.Pi_root, sl_Buffer, standard)
+        analyze_Pi_graph_dump(self.file_no, self.Pi_root, sl_Buffer, standard)
 
     def report_performance(self):
         if self.Pi_root is None:

baselines/MCTS/save_1/sl_buffer1.py
@@ -4,58 +4,50 @@ import scipy.sparse as sp
 
 class slBuffer_oneFile(object):
     def __init__(self, size, fileNo):
-        """Create sl_buffer for one file at fileNo, that use sparse representation for saving memories.
+        """Create sl_buffer that use sparse representation for saving memories.
 
         Parameters
         ----------
         size: int
             Max number of transitions to store in the buffer. When the buffer
             overflows the old memories are dropped.
-        fileNo: int
-            the file number for this buffer
         """
-        self._storage = []
         self._maxsize = size
-        self._next_idx = 0
         self.fileNo = fileNo
-        # this needs to be updated when data is added or removed. It helps calculate the actual score for each state/Pi pair
-        self.mean_step = 0
+        # storage related fields
+        self._storage = []
+        self._nrepeat = []
+        self._next_idx = 0
+        self._prob = None
 
     def __len__(self):
         return len(self._storage)
 
-    def add(self, obs, Pi, step):
-        data = (obs, Pi, step)
+    def add(self, obs, Pi, score, repeat):
+        self._prob = None
+        # obs is alread 2d sparse array
+        data = (obs, Pi, score)
 
-        if self._next_idx >= len(self._storage): # adding new data at new space!
-            current_count = len(self._storage)
+        if self._next_idx >= len(self._storage):
             self._storage.append(data)
-            self.mean_step *= current_count * 1.0 / (current_count + 1)
-            self.mean_step += step * 1.0 / (current_count + 1)
-        else: # adding new data at old space, while removing an old data!
-            _, _, old_step = self._storage[self._next_idx]
+            self._nrepeat.append(repeat)
+        else:
             self._storage[self._next_idx] = data
-            self.mean_step += (step - old_step) * 1.0 / self._maxsize
-
+            self._nrepeat[self._next_idx] = repeat
         self._next_idx = (self._next_idx + 1) % self._maxsize
-
-    def _get_score(self, step):
-    	return np.tanh((self.mean_step - step) * 3.0 / self.mean_step)
 
     def _encode_sample(self, idxes):
         obses, Pis, scores = [], [], []
         for i in idxes:
             data = self._storage[i]
-            obs, Pi, step = data
+            obs, Pi, score = data
             # Comments by Fei: effort to convert observation (2D sparse) back to 3D numpy"
             obs_2d = obs.toarray()
-            obs_3d = np.reshape(obs_2d, [-1, int(obs_2d.shape[1] / 2), 2])
-
+            obs_3d = np.reshape(obs_2d, [-1, int(obs_2d.shape[1] / 2), 2]) 
+            
             obses.append(obs_3d)
             Pis.append(Pi)
-            # Comments by Fei: effort to transform step into score
-            scores.append(self._get_score(step))
-
+            scores.append(score)
         return np.array(obses, dtype = np.float32), np.array(Pis, dtype = np.float32), np.array(scores, dtype = np.float32)
 
     def sample(self, batch_size):
@@ -75,7 +67,9 @@ class slBuffer_oneFile(object):
         score_batch: np.array
             rewards received as results of executing act_batch
         """
-        idxes = np.random.choice(len(self._storage), batch_size)
+        if self._prob is None:
+            self._prob = np.array(self._nrepeat) / sum(self._nrepeat)
+        idxes = np.random.choice(len(self._storage), batch_size, p = self._prob)
         return self._encode_sample(idxes)
 
 class slBuffer_allFile(object):
@@ -92,11 +86,11 @@ class slBuffer_allFile(object):
         self.bufferList = []
         for i in range(n_files): 
             self.bufferList.append(slBuffer_oneFile(self.eachSize, i))
-        self.sample_round = -1
+        self.sample_round = -1 # this is the round-robin index for sample from the list of slBuffer_oneFile
         self.sample_list = np.zeros(self.n_files, dtype = np.bool)
 
-    def add(self, fromWhichFile, obs, Pi, step):
-        self.bufferList[fromWhichFile].add(obs, Pi, step)
+    def add(self, fromWhichFile, obs, Pi, step, repeat):
+        self.bufferList[fromWhichFile].add(obs, Pi, step, repeat)
         self.sample_list[fromWhichFile] = True
 
     def sample(self, batch_size):
@@ -104,17 +98,6 @@ class slBuffer_allFile(object):
         self.sample_round += 1
         self.sample_round %= self.n_files
         while not self.sample_list[self.sample_round]:
-        	self.sample_round += 1
-        	self.sample_round %= self.n_files
+            self.sample_round += 1
+            self.sample_round %= self.n_files
         return self.bufferList[self.sample_round].sample(batch_size)
-
-import sys, pickle
-if __name__ == '__main__':
-    filename = sys.argv[1]
-    with open(filename, "rb") as f:
-        buffer1 = pickle.load(f)
-    X, Y, Z = buffer1.sample(6)
-    for i in range(6):
-        print(np.sum(X[i]))
-        print(Y[i])
-        print(Z[i])

baselines/MCTS/save_sim/sl_buffer_sim.py
@@ -17,45 +17,66 @@ class slBuffer_oneFile(object):
         self._maxsize = size
         self.fileNo = fileNo
 
-        # actual store of data and repeats
-        self._storage = []
-        self._next_idx = 0 # the next index to add samples (for override old samples)
-        self._nrepeat = []
-        self.sum_nrepeat = 0 # this number tracks the sum of _nrepeat (needs update when samples are added or removed)
+        self._storage = [] # actual store of data 
+        self._next_idx = 0 # index to write to
         
-        # this is the probability distribution of _nrepeat 
-        self._prob = None # (it is None at initialization and when new samples are added)
-        # this helps calculate the actual score for each state/Pi pair
-        self.mean_step = 0 # (needs to be updated when samples are added or removed). 
+        # this needs to be updated when data is added or removed. It helps calculate the actual score for each state/Pi pair
+        self.mean_step = 0
+        self.n_play = 0
+        # VERY IMPORTANT:: mean_step should be averaged by plays, not by steps.
+        # it means that if I have two plays, the first play toke 4 steps and the second play took 8 steps.
+        # the mean_step should be 6.
+        # However I will have 4 additions to the buffer that says steps-4 
+        #                 and 8 additions to the buffer that says steps-8
+        # If I mistakenly did average by steps, I will get mean_step = (4 * 4 + 8 * 8) / (4 + 8) = 6.667
+        # which bias the mean_step to a larger value
+
+        # However it seems hard to keep accurate value of mean_step by play, because when storage is full, 
+        # data are taken out by step not by play!! TRICKY: SOLVE LATER. For now, let's just assume that no data are removed
 
     def __len__(self):
         return len(self._storage)
 
-    def add(self, obs, Pi, step, repeat):
-        self._prob = None
-        # obs is already 2d sparse array 
+    def add(self, obs, Pi, step):
+        assert False, "deprecated function"
         data = (obs, Pi, step)
 
         if self._next_idx >= len(self._storage): # adding new data at new space!
+            current_count = len(self._storage)
             self._storage.append(data)
-            self._nrepeat.append(repeat)
-            old_sum_nrepeat = self.sum_nrepeat # save old sum_nrepeat
-            self.sum_nrepeat += repeat
-            self.mean_step *= (old_sum_nrepeat / self.sum_nrepeat)
-            self.mean_step += (step * repeat   / self.sum_nrepeat)
-            
+            self.mean_step *= current_count * 1.0 / (current_count + 1)
+            self.mean_step += step * 1.0 / (current_count + 1)
         else: # adding new data at old space, while removing an old data!
             _, _, old_step = self._storage[self._next_idx]
-            old_repeat = self._nrepeat[self._next_idx]
             self._storage[self._next_idx] = data
-            self._nrepeat[self._next_idx] = repeat
-            old_sum_nrepeat = self.sum_nrepeat
-            self.sum_nrepeat = self.sum_nrepeat + repeat - old_repeat
-            self.mean_step *= (old_sum_nrepeat / self.sum_nrepeat)
-            self.mean_step += ((step * repeat - old_step * old_repeat) / self.sum_nrepeat)
+            self.mean_step += (step - old_step) * 1.0 / self._maxsize
 
         self._next_idx = (self._next_idx + 1) % self._maxsize
 
+    def add_a_play(self, list_of_Pi_node, level):
+        """
+            this function add a play of state/Pi/steps. 
+            assume that the input is a list of Pi_nodes for one play.
+        """
+        # update self.n_play and self.mean_step
+        self.n_play += 1
+        self.mean_step *= ((self.n_play - 1) / self.n_play)
+        self.mean_step += level / self.n_play
+        # put data in storage
+        for node in list_of_Pi_node:
+            self.add_uncheck(node.state, node.Pi, level)
+
+    def add_uncheck(self, obs, Pi, step):
+        """
+            this function add state without updating the self.mean_step
+        """
+        data = (obs, Pi, step)
+        if self._next_idx >= len(self._storage): # adding new data at new space!
+            self._storage.append(data)
+        else: # adding new data at old space, while removing an old data!
+            self._storage[self._next_idx] = data
+        self._next_idx = (self._next_idx + 1) % self._maxsize
+        
     def _get_score(self, step):
     	return np.tanh((self.mean_step - step) * 3.0 / self.mean_step)
 
@@ -92,9 +113,7 @@ class slBuffer_oneFile(object):
         score_batch: np.array
             rewards received as results of executing act_batch
         """
-        if self._prob is None:
-        	self._prob = np.array(self._nrepeat) / sum(self._nrepeat)
-        idxes = np.random.choice(len(self._storage), batch_size, p = self._prob)
+        idxes = np.random.choice(len(self._storage), batch_size)
         return self._encode_sample(idxes)
 
 class slBuffer_allFile(object):
@@ -111,11 +130,12 @@ class slBuffer_allFile(object):
         self.bufferList = []
         for i in range(n_files): 
             self.bufferList.append(slBuffer_oneFile(self.eachSize, i))
-        self.sample_round = -1 # this is the round-robin index for sample from the list of slBuffer_oneFile
+        self.sample_round = -1
         self.sample_list = np.zeros(self.n_files, dtype = np.bool)
 
-    def add(self, fromWhichFile, obs, Pi, step, repeat):
-        self.bufferList[fromWhichFile].add(obs, Pi, step, repeat)
+    def add(self, fromWhichFile, obs, Pi, step):
+        assert False, "deprecated function"
+        self.bufferList[fromWhichFile].add(obs, Pi, step)
         self.sample_list[fromWhichFile] = True
 
     def sample(self, batch_size):
@@ -123,9 +143,16 @@ class slBuffer_allFile(object):
         self.sample_round += 1
         self.sample_round %= self.n_files
         while not self.sample_list[self.sample_round]:
-            self.sample_round += 1
-            self.sample_round %= self.n_files
+        	self.sample_round += 1
+        	self.sample_round %= self.n_files
         return self.bufferList[self.sample_round].sample(batch_size)
+
+    def add_a_play(self, fromWhichFile, list_of_Pi_node, level):
+        """
+            this function calls add_a_play of one of the bufferList
+        """
+        self.bufferList[fromWhichFile].add_a_play(list_of_Pi_node, level)
+        self.sample_list[fromWhichFile] = True
 
 import sys, pickle
 if __name__ == '__main__':